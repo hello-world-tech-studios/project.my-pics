fc367111004c60d273d1a7294502ea89ea130f3c
Hack Reactor Student <student@hackreactor.com>
2017-06-22 16:23:51 -0500

    Added functionality for displaying picture clicked

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
 
   /* Your Javascript Here */
-
+  console.log("page is loaded");
+  $(".pictureBoxes").click(function(event){
+    
+    var pictureClicked = event.target.src;
+    console.log(event.target.src);
+    $("#pictureDisplay").append(`<img src="${pictureClicked}"/>`);
+  });
 });
